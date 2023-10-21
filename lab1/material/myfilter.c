62bf5149684384da69ad684d47868af9fd59bc6f
Edoardo Manfredi <edoardom59@tutanota.com>
2023-10-21 19:30:33 +0200

    fix(filter-c): initialize bi with correct values

--- lab1/material/myfilter.c
+++ lab1/material/myfilter.c
@@ -7,8 +7,8 @@
 #define NB 9 /// number of bits
 #define SHAMT 8 /// shift amount
 
-const int bi[NT]={8, 111, 8}; /// b array
-const int ai[NT-1]={0, 0}; /// a array
+int bi[NT]; /// b array
+int ai[NT-1]; /// a array
 
 /// Perform fixed point filtering assming direct form I
 ///\param x is the new input sample
@@ -40,30 +40,52 @@ int myfilter(int x)
   /// Moving average part
   y = 0;
   for (i=0; i<NT; i++)
-    y += ((sx[i]*bi[i]) >> SHAMT) << (SHAMT-NB+1);    
-  /// Auto regressive part
-  for (i=0; i<NT-1; i++)
-    y -= ((sy[i]*ai[i]) >> SHAMT) << (SHAMT-NB+1);    
+    y += ((sx[i]*bi[i]) >> SHAMT) << (SHAMT-NB+1);
 
   /// update the y shift register
   for (i=NT-2; i>0; i--)
     sy[i] = sy[i-1];
   sy[0] = y;
- 
+
   return y;
 }
 
+const char bi_file_name[7] = "bi.txt";
+
 int main (int argc, char **argv)
 {
-  FILE *fp_in;
-  FILE *fp_out;
-  FILE *fp_out_m;
+  FILE *fp_in = NULL;
+  FILE *fp_bi = NULL;
+  FILE *fp_out = NULL;
+  FILE *fp_out_m = NULL;
 
   int x;
   int y;
   int count = 0; /// output sample
-  long int somma = 0; /// output sample
-  int e; /// matlab results
+  double somma = 0; /// output sample
+  int expected; /// matlab results
+
+  // Initialize bi with values written in bi.txt
+  int iter_bi = 0; // index for bi
+
+  fp_bi = fopen(bi_file_name, "r");
+  if (fp_bi == NULL) {
+    printf("Error: cannot open %s\n", bi_file_name);
+    exit(2);
+  }
+
+  while (iter_bi <= NT && (fscanf(fp_bi, "%d", &bi[iter_bi++]) == 1));
+
+  if (--iter_bi != NT) {
+    printf("Error: mismatch number of elements\n");
+    printf("Expected: %d\t\tRead: %d\n", NT, iter_bi);
+    exit(1);
+  }
+
+  fclose(fp_bi);
+
+  // initialize ai vector with zeros
+  /* for (int i = 0; i < NT; ai[i++] = 0); */
 
   /// check the command line
   if (argc != 3)
@@ -71,14 +93,14 @@ int main (int argc, char **argv)
     printf("Use: %s <input_file> <output_file>\n", argv[0]);
     exit(1);
   }
-  
+
   /// open files
   fp_in = fopen(argv[1], "r");
-  if (fp_in == NULL)
-  {
+  if (fp_in == NULL) {
     printf("Error: cannot open %s\n");
     exit(2);
   }
+
   fp_out = fopen(argv[2], "w");
   fp_out_m = fopen("./resultsm.txt", "r");
 
@@ -91,20 +113,19 @@ int main (int argc, char **argv)
 
   /// get samples and apply filter
   fscanf(fp_in, "%d", &x);
-  fscanf(fp_out_m, "%d", &e);
+  fscanf(fp_out_m, "%d", &expected);
   do{
     y = myfilter(x);
-    somma += pow(y - e, 2);
+    somma += pow(y - expected, 2);
     count++;
 
-    printf("%d\n", somma);
     fscanf(fp_in, "%d", &x);
-    fscanf(fp_out_m, "%d", &e);
+    fscanf(fp_out_m, "%d", &expected);
 
     fprintf(fp_out,"%d\n", y);
   } while (!feof(fp_in));
 
-  printf("somma: %d\n", sqrt(somma / count));
+  printf("fixed-point vs pseudo-fixed-point\nrmsd: %f\n", sqrt(somma / count));
 
   fclose(fp_in);
   fclose(fp_out);
