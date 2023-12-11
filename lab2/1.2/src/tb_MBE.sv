fcf69ec3bb5ea40671f0cc36c241d6e0628160ea
Edoardo Manfredi <edoardom59@tutanota.com>
2023-12-11 15:47:50 +0100

    fix(tb): avoid underflow and add stop conditions

--- lab2/1.2/src/tb_MBE.sv
+++ lab2/1.2/src/tb_MBE.sv
@@ -1,33 +1,40 @@
 module tb_MBE;
 
   parameter NBIT = 11;
-  reg [10:0] a;
-  reg [10:0] b;
+  // one additional bit to avoid overflow
+  reg [11:0] a;
+  reg [11:0] b;
   reg [21:0] result;
   reg [21:0] expected_result;
+  reg end_i;
+
 
   MBE uut (
     .result(result),
-    .a(a),
-    .b(b)
+    .a(a[10:0]),
+    .b(b[10:0])
   );
 
   reg clk = 0;
   always #5 clk = ~clk;
 
   initial begin
-    for (a = 11'b00000000000; a <= 11'b11111111111; a = a + 1) begin
-      for (b = 11'b00000000000; b <= 11'b11111111111; b = b + 1) begin
-         expected_result = a * b;
+    end_i = 0;
+    for (a = 12'b000000000000; a < 12'b100000000000; a = a + 1) begin
+      for (b = 12'b000000000000; b < 12'b100000000000; b = b + 1) begin
+         expected_result = a[10:0] * b[10:0];
          #10;
      end
     end
+    end_i = 1;
+    $stop;
   end
 
   
   always @(posedge clk) begin
     if (result !== expected_result) begin
       $display("Mismatch: a=%0d, b=%0d, result=%0d, expected=%0d", a, b, result, expected_result);
+      $stop;
     end
   end
 
