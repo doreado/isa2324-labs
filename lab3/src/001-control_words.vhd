d9701d5d382588cc20806df3f23b62f3845f6566
Edoardo Manfredi <edoardom59@tutanota.com>
2024-02-15 19:09:51 +0100

    fix(cw): update control words

--- lab3/src/001-control_words.vhd
+++ lab3/src/001-control_words.vhd
@@ -22,9 +22,9 @@ package control_words is
         cmp_sel    : std_logic; -- Select how to extend operands in branch comparator (1: signed, 0: unsigned)
         imm_sel    : imm_t;     -- Select the second operand used to compute the target address 
         is_jalr    : std_logic;
-        MUX_COND_SEL : std_logic_vector(1 downto 0); -- MUX_COND selection signal
+        MUX_COND_SEL : std_logic_vector(1 downto 0); -- used to distinguish bge, blt, jal
         MUX_SIGNED : std_logic; -- MUX_SIGNED selection signal
-        MUX_J_SEL  : std_logic; -- MUX_J_SEL selection signal
+        MUX_J_SEL  : std_logic; -- MUX_J_SEL selection signal TODO: useless
         MUX_R_SEL  : std_logic_vector(1 downto 0); -- selects between RD, RS and LR
     end record decode_cw_t;
 
@@ -70,9 +70,9 @@ package control_words is
             RF_RESET     => '0',
             RF_ENABLE    => '1',
             RF_RD1       => '1',
-            RF_RD2       => '1',
+            RF_RD2       => '0',
             cmp_sel      => '0',
-            imm_sel      => zero,
+            imm_sel      => i_imm,
             is_jalr      => '0',
             MUX_COND_SEL => "00",
             MUX_SIGNED   => '1',
@@ -101,9 +101,9 @@ package control_words is
             RF_RESET     => '0',
             RF_ENABLE    => '1',
             RF_RD1       => '1',
-            RF_RD2       => '1',
+            RF_RD2       => '0',
             cmp_sel      => '0',
-            imm_sel      => zero,
+            imm_sel      => u_imm,
             is_jalr      => '0',
             MUX_COND_SEL => "00",
             MUX_SIGNED   => '1',
@@ -132,9 +132,9 @@ package control_words is
             RF_RESET     => '0',
             RF_ENABLE    => '1',
             RF_RD1       => '1',
-            RF_RD2       => '1',
+            RF_RD2       => '0',
             cmp_sel      => '0',
-            imm_sel      => zero,
+            imm_sel      => s_imm,
             is_jalr      => '0',
             MUX_COND_SEL => "00",
             MUX_SIGNED   => '1',
@@ -163,9 +163,9 @@ package control_words is
             RF_RESET     => '0',
             RF_ENABLE    => '1',
             RF_RD1       => '1',
-            RF_RD2       => '1',
+            RF_RD2       => '0',
             cmp_sel      => '0',
-            imm_sel      => zero,
+            imm_sel      => i_imm,
             is_jalr      => '0',
             MUX_COND_SEL => "00",
             MUX_SIGNED   => '1',
@@ -250,44 +250,13 @@ package control_words is
         )
     );
 
-    -- J
-    constant J_CW : cw_t := (
-        decode  => (
-            RF_RESET     => '0',
-            RF_ENABLE    => '1',
-            RF_RD1       => '1',
-            RF_RD2       => '1',
-            cmp_sel      => '0',
-            imm_sel      => zero,
-            is_jalr      => '0',
-            MUX_COND_SEL => "11",
-            MUX_SIGNED   => '1',
-            MUX_J_SEL    => '1',
-            MUX_R_SEL    => "00"
-        ),
-        execute => (
-            ALU_OP    => alu_add,
-            MUX_A_SEL => '0',
-            MUX_B_SEL => '1'
-        ),
-        memory  => (
-            LMD_EN            => '0',
-            DRAM_ENABLE       => '0',
-            DRAM_READNOTWRITE => '1'
-        ),
-        wb      => (
-            RF_WR       => '0',
-            MUX_LMD_SEL => "00"
-        )
-    );
-
     -- JAL
     constant JAL_CW : cw_t := (
         decode  => (
             RF_RESET     => '0',
             RF_ENABLE    => '1',
-            RF_RD1       => '1',
-            RF_RD2       => '1',
+            RF_RD1       => '0',
+            RF_RD2       => '0',
             cmp_sel      => '0',
             imm_sel      => uj_imm,
             is_jalr      => '0',
@@ -318,7 +287,7 @@ package control_words is
             RF_RESET     => '0',
             RF_ENABLE    => '1',
             RF_RD1       => '1',
-            RF_RD2       => '1',
+            RF_RD2       => '0',
             cmp_sel      => '0',
             imm_sel      => i_imm,
             is_jalr      => '1',
@@ -330,37 +299,6 @@ package control_words is
         execute => (
             ALU_OP    => alu_add,
             MUX_A_SEL => '1',
-            MUX_B_SEL => '0'
-        ),
-        memory  => (
-            LMD_EN            => '0',
-            DRAM_ENABLE       => '0',
-            DRAM_READNOTWRITE => '1'
-        ),
-        wb      => (
-            RF_WR       => '0',
-            MUX_LMD_SEL => "01"
-        )
-    );
-
-    -- BGE
-    constant BGE_CW : cw_t := (
-        decode  => (
-            RF_RESET     => '0',
-            RF_ENABLE    => '1',
-            RF_RD1       => '1',
-            RF_RD2       => '1',
-            cmp_sel      => '1',
-            imm_sel      => sb_imm,
-            is_jalr      => '0',
-            MUX_COND_SEL => "01",
-            MUX_SIGNED   => '1',
-            MUX_J_SEL    => '0',
-            MUX_R_SEL    => "00"
-        ),
-        execute => (
-            ALU_OP    => alu_add,
-            MUX_A_SEL => '0',
             MUX_B_SEL => '1'
         ),
         memory  => (
@@ -369,44 +307,13 @@ package control_words is
             DRAM_READNOTWRITE => '1'
         ),
         wb      => (
-            RF_WR       => '0',
+            RF_WR       => '1',
             MUX_LMD_SEL => "01"
         )
     );
 
-    -- BGEU
-    constant BGEU_CW : cw_t := (
-        decode     => (
-        RF_RESET   => '0',
-        RF_ENABLE  => '1',
-        RF_RD1     => '1',
-        RF_RD2     => '1',
-        cmp_sel    => '1',
-        imm_sel    => sb_imm,
-        is_jalr    => '0',
-        MUX_COND_SEL   => "01",
-        MUX_SIGNED => '0',
-        MUX_J_SEL  => '0',
-        MUX_R_SEL  => "00"
-        ),
-        execute        => (
-        ALU_OP         => alu_add,
-        MUX_A_SEL      => '0',
-        MUX_B_SEL      => '1'
-        ),
-        memory            => (
-        LMD_EN            => '0',
-        DRAM_ENABLE       => '0',
-        DRAM_READNOTWRITE => '1'
-        ),
-        wb          => (
-        RF_WR       => '0',
-        MUX_LMD_SEL => "01"
-        )
-    );
-
-    -- BLT
-    constant BLT_CW : cw_t := (
+    -- BGE
+    constant BGE_CW : cw_t := (
         decode  => (
             RF_RESET     => '0',
             RF_ENABLE    => '1',
@@ -443,7 +350,7 @@ package control_words is
             RF_ENABLE    => '1',
             RF_RD1       => '1',
             RF_RD2       => '1',
-            cmp_sel      => '1',
+            cmp_sel      => '0',
             imm_sel      => sb_imm,
             is_jalr      => '0',
             MUX_COND_SEL => "01",
@@ -463,6 +370,37 @@ package control_words is
         ),
         wb      => (
             RF_WR       => '0',
+            MUX_LMD_SEL => "01"
+        )
+    );
+
+    -- LUI
+    constant LUI_CW : cw_t := (
+        decode  => (
+            RF_RESET     => '0',
+            RF_ENABLE    => '1',
+            RF_RD1       => '0',
+            RF_RD2       => '0',
+            cmp_sel      => '0',
+            imm_sel      => u_imm,
+            is_jalr      => '0',
+            MUX_COND_SEL => "01",
+            MUX_SIGNED   => '0',
+            MUX_J_SEL    => '0',
+            MUX_R_SEL    => "00"
+        ),
+        execute => (
+            ALU_OP    => alu_add,
+            MUX_A_SEL => '0',
+            MUX_B_SEL => '1'
+        ),
+        memory  => (
+            LMD_EN            => '0',
+            DRAM_ENABLE       => '0',
+            DRAM_READNOTWRITE => '1'
+        ),
+        wb      => (
+            RF_WR       => '1',
             MUX_LMD_SEL => "01"
         )
     );
