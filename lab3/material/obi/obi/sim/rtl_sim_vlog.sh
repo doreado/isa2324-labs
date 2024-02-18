c8c66a147766782d17916bd978bfc85f81657e65
alessandrovargiu <alessandrovargiu98@gmail.com>
2024-02-18 18:25:41 +0100

    memfakewrap not working

--- lab3/material/obi/obi/sim/rtl_sim_vlog.sh
+++ lab3/material/obi/obi/sim/rtl_sim_vlog.sh
@@ -10,11 +10,15 @@ tres="ps"
 
 #vcom -work mem_wrap -nodebug ../src/mem_wrap_fake2.0.vhd
 
+
 vcom -work work ../tb/req_gen.vhd
 vcom -work work ../tb/req_gen_fast.vhd
 
 vcom -work work ../tb/clk_gen.vhd
 vcom -work work ../tb/data_dumper.vhd
+vcom -work work ../../../../src/000-globals.vhd
+vcom -work work ../../../../src/a.e-LoadStoreUnit.vhd
+#vcom -work work ../../../../src/memories/rw/rwmem.vhd
 vlog -work work ../tb/tb_mem_wrap_fake.v
 
-vsim -t ${tres} -L ./mem_wrap -do wave2.0.do work.tb_mem_wrap_fake -voptargs=+acc
+vsim -c -t ${tres} -L ./mem_wrap -do wave2.0.do work.tb_mem_wrap_fake -voptargs=+acc
