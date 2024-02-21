52b1d026dc168eb694bb34b43aeb6524d4156c4e
alessandrovargiu <alessandrovargiu98@gmail.com>
2024-02-21 23:54:16 +0100

    OBI interface

--- lab3/src/a.e-OBI_interface.sv
+++ lab3/src/a.e-OBI_interface.sv
@@ -4,72 +4,73 @@ module obi_interface(
     input logic clk,
     input logic rst,
     // from CPU
-    input logic proc_req,
-    input logic [ADDRESS_WIDTH-1:0] addr,
+    input logic IRAM_proc_req,
+    input logic DRAM_proc_req,
+    input logic [ADDRESS_WIDTH-1:0] IRAM_addr,
+    input logic [ADDRESS_WIDTH-1:0] DRAM_addr,
     input logic [DATA_WIDTH-1:0] wdata,
     input logic web,        // write enable -  active low
-    input logic mem_sel,       // 0 - CODE, 1 - DATA
+    // from MEMORY
+    input logic IRAM_rdy,
+    input logic DRAM_rdy,
+    input logic [DATA_WIDTH-1:0] IRAM_rdata,
+    input logic [DATA_WIDTH-1:0] DRAM_rdata,
+    input logic IRAM_valid_in,
+    input logic DRAM_valid_in,
+    // to MEMORY
+    output logic [ADDRESS_WIDTH-1:0] IRAM_addr_out,
+    output logic [ADDRESS_WIDTH-1:0] DRAM_addr_out,
+    output logic web_out,
+    output logic wdata_out,
     // to CPU
-    output logic [DATA_WIDTH-1:0] dout,
-    output logic valid
-    
+    output logic [DATA_WIDTH-1:0] IRAM_dout,
+    output logic [DATA_WIDTH-1:0] DRAM_dout,
+    output logic IRAM_valid_out,
+    output logic DRAM_valid_out
 );
 
-    wire csb0, csb1;        // chip select (USE AS ENABLE SIGNAL FOR MEMORIES)
-    wire [DATA_WIDTH-1:0] IRAM_output, DRAM_output;
-    wire [DATA_WIDTH-1:0] LSU_addr, F0_ADDR; 
-    wire [DATA_WIDTH-1:0] LSU_wdata;   
-    wire LSU_we;
-
+    
+    /*
     always @* begin
         csb0 = (mem_sel == 1'b0) ? 1'b0 : 1'b1;
         csb1 = (mem_sel == 1'b0) ? 1'b1 : 1'b0;
     end
-
+    */
     Fetcher F_0 (
         .clk(clk),
         .rst(rst),
-        .proc_req_in(proc_req),
-        .addr_in(addr),
-        .mem_rdy(1),
-        // output
-        .rdata(IRAM_output),
-        .valid_in(1),
+        .proc_req_in(IRAM_proc_req),
+        .addr_in(IRAM_addr),
+        .mem_rdy(IRAM_rdy),
+        // input
+        .rdata(IRAM_rdata),
+        .valid_in(IRAM_valid_in),           // 1 if using ssram
         // to CPU
-        .data_out(dout),
-        .valid_out(valid),
+        .data_out(IRAM_dout),
+        .valid_out(IRAM_valid_out),
         // to memories
-        .addr_out(F0_ADDR)
+        .addr_out(IRAM_addr_out)
     );
 
     LoadStoreUnit LSU (
         .clk(clk),
         .rst(rst),
-        .proc_req_in(proc_req),
-        .addr_in(addr),
+        .proc_req_in(DRAM_proc_req),
+        .addr_in(DRAM_addr),
         .wdata_in(wdata),
         .we_in(web),
-        .mem_rdy(1),
+        .mem_rdy(DRAM_rdy),
         // output
-        .rdata(DRAM_output),
-        .valid_in(1),
+        .rdata(DRAM_rdata),
+        .valid_in(DRAM_valid_in),
         // to CPU         
-        .data_out(dout),
-        .valid_out(valid),
+        .data_out(DRAM_dout),
+        .valid_out(DRAM_valid_out),
         // to memories
-        .addr_out(LSU_addr),
-        .we_out(LSU_we),
-        .wdata_out(LSU_wdata)
+        .addr_out(DRAM_addr_out),
+        .we_out(web_out),
+        .wdata_out(wdata_out)
     );
     
-    // INSTANTIATE MEMORIES HERE (SSRAM or mem_fake_wrapper)
-
-    /*
-    ssram_wrap memories (
-        .clk(clk),
-        .csb(mem_sel),
-        .web()
-    );
-    */
 
 endmodule
