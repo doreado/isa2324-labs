763a9f213a7d498fd0436eb33f5862203be89894
Edoardo Manfredi <edoardom59@tutanota.com>
2024-04-15 19:48:34 +0200

    feat: instantiate fpu components in tb

--- lab4/material/es4part2_fpu/src/DUT.sv
+++ lab4/material/es4part2_fpu/src/DUT.sv
@@ -1,6 +1,60 @@
 module DUT(dut_if.port_in in_inter, dut_if.port_out out_inter, output enum logic [1:0] {INITIAL,WAIT,SEND} state);
     
-    adder adder_under_test(.A(in_inter.A),.B(in_inter.B),.OUT(out_inter.data));
+    wire clk_i;
+    wire rst_ni;
+    wire [NUM_OPERANDS-1:0][WIDTH-1:0] operands_i;
+    var fpnew_pkg::roundmode_e rnd_mode_i;
+    var fpnew_pkg::operation_e op_i;
+    var logic op_mod_i;
+    var fpnew_pkg::fp_format_e src_fmt_i;
+    var fpnew_pkg::fp_format_e dst_fmt_i;
+    var fpnew_pkg::int_format_e int_fmt_i;
+    var logic vectorial_op_i;
+    var TagType tag_i;
+    wire in_valid_i;
+    wire in_ready_o;
+    var logic flush_i;
+    wire [WIDTH-1:0] result_o;
+    var fpnew_pkg::status_t status_o;
+    wire tag_o;
+    wire out_valid_o;
+    var logic out_ready_i;
+    wire busy_o;
+    wire end_sim;
+
+    assign rnd_mode_i = fpnew_pkg::RNE;
+    assign op_i = fpnew_pkg::MUL;
+    assign op_mod_i = 0;
+    assign src_fmt_i = fpnew_pkg::FP16;
+    assign dst_fmt_i = fpnew_pkg::FP16;
+    assign int_fmt_i = fpnew_pkg::INT16;
+    assign vectorial_op_i = 0;
+    assign tag_i = 0;
+    assign flush_i = 0;
+    assign out_ready_i = out_valid_o;
+
+    fpnew_top fpu_under_test(
+      .clk_i(in_inter.clk),
+      .rst_ni(!in_inter.rst),
+      .operands_i(in_inter.operands),
+      .rnd_mode_i,
+      .op_i,
+      .op_mod_i,
+      .src_fmt_i,
+      .dst_fmt_i,
+      .int_fmt_i,
+      .vectorial_op_i,
+      .tag_i,
+      .in_valid_i(in_inter.valid),
+      .in_ready_o,
+      .flush_i,
+      .status_o,
+      .result_o(out_inter.result), 
+      .tag_o,
+      .out_valid_o,
+      .busy_o,
+      .out_ready_i,
+    );
 
     always_ff @(posedge in_inter.clk)
     begin
--- lab4/material/es4part2_fpu/src/dut_if.sv
+++ lab4/material/es4part2_fpu/src/dut_if.sv
@@ -1,9 +1,17 @@
-interface dut_if(input clk, rst);
-    logic [31:0] A, B;
-    logic [31:0] data;
-    logic valid, ready;
-    
-    modport port_in (input clk, rst, A, B, valid, output ready);
-    modport port_out (input clk, rst, output valid, data, ready);
-endinterface
+interface dut_if #(
+  parameter fpnew_pkg::fpu_features_t       Features       = fpnew_pkg::RV16F,		   
+  parameter fpnew_pkg::fpu_implementation_t Implementation = fpnew_pkg::ISA_PIPE,
+  parameter type                            TagType        = logic,
+  // Do not change
+  localparam int unsigned WIDTH        = Features.Width,
+  localparam int unsigned NUM_OPERANDS = 3
+ ) (input clk, rst);
+
+    input logic [NUM_OPERANDS-1:0][WIDTH-1:0] operands_i,
+      logic [WIDTH-1:0]
+      logic valid, ready;
+
+      modport port_in (input clk, rst, operands, B, valid, output ready);
+      modport port_out (input clk, rst, output result, valid);
+  endinterface
 
