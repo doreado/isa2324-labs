b756a8a772bdf277872ac8287de29071f1d4e21c
Edoardo Manfredi <edoardom59@tutanota.com>
2024-04-15 19:48:34 +0200

    wip(fpu_uvm): read samples for building packets

--- lab4/material/es4part2_fpu/tb/sequence_in.sv
+++ lab4/material/es4part2_fpu/tb/sequence_in.sv
@@ -1,4 +1,8 @@
 class sequence_in extends uvm_sequence #(packet_in);
+
+    rand bit [15:0] sample[24];
+    rand bit [15:0] results[12];
+
     `uvm_object_utils(sequence_in)
 
     function new(string name="sequence_in");
@@ -8,11 +12,15 @@ class sequence_in extends uvm_sequence #(packet_in);
     task body;
         packet_in tx;
 
-        forever begin
-            tx = packet_in::type_id::create("tx");
-            start_item(tx);
-            assert(tx.randomize());
-            finish_item(tx);
+        $readmemb("./refmod/samples.txt", sample);
+        $readmemb("./refmod/result_c.txt", results);
+
+        for (int i = 0; i < 12; i+=2) begin
+          tx = packet_in::type_id::create("tx");
+          start_item(tx);
+          tx.A = sample[i];
+          tx.B = sample[i + 1];
+          finish_item(tx);
         end
     endtask: body
 endclass: sequence_in
